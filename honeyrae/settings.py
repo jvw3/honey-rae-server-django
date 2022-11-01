82fd79b4a6a3fd2ac09d8312c89adeff56d2026e
Jonathan Woodard <jvw.226@gmail.com>
2022-11-01 13:48:03 -0500

    updated .env to remove secret key

--- honeyrae/settings.py
+++ honeyrae/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for honeyrae project.
 
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fh#*u^ygtu5ld8cd*&gf5w*@*m7sa+_#+1mr!vooo^v3o^fcdi'
+SECRET_KEY = os.environ.get('MY_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
